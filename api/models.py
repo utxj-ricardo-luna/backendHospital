7f35dd8cb0c0f40a1bddb681c4645fa524b20605
jjuancoo <161867767+jjuancoo@users.noreply.github.com>
2024-03-19 14:18:53 -0600

    Creando las tablas y actualizando el models
    Se crearon las tablas SQL para complementar los modelos

--- api/models.py
+++ api/models.py
@@ -22,5 +22,32 @@ class c_rol(models.Model):
     #TextField(blanck=True)
 	def __str__(self):
 		return self.ro_nombre
-		
+
+#Tabla estudio de
+class c_estudio(models.Model):
+	d_nombre = models.CharField(max_length=50)
+	d_descripcion = models.CharField(max_length=250)
+
+
+class Estudio(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=500)
+
+class Cita(models.Model):
+    fecha = models.DateField()
+    hora = models.TimeField()
+    paciente_id = models.IntegerField()  # Se asume que esto es un ID de paciente
+    medico_id = models.IntegerField()  # Se asume que esto es un ID de médico
+    #tipo_estudio = models.ForeignKey(Estudio, on_delete=models.CASCADE, related_name='citas')
+
+class ResultadoEstudio(models.Model):
+    estudio = models.ForeignKey(Estudio, on_delete=models.CASCADE)
+    cita = models.ForeignKey(Cita, on_delete=models.CASCADE)
+    resultado = models.CharField(max_length=500)
+    imagen = models.BinaryField()
+
+class Consumible(models.Model):
+    nombre = models.CharField(max_length=100)
+    cantidad = models.IntegerField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
 
